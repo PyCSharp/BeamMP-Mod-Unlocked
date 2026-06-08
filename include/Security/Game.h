f73c0aabb324699b98751cffe8d2ac94da9ba42d
PyCSharp <210414190+PyCSharp@users.noreply.github.com>
2026-06-08 17:52:08 +0200

    Delete include directory

--- include/Security/Game.h
+++
@@ -1,8 +0,0 @@
-/*
- Copyright (C) 2024 BeamMP Ltd., BeamMP team and contributors.
- Licensed under AGPL-3.0 (or later), see <https://www.gnu.org/licenses/>.
- SPDX-License-Identifier: AGPL-3.0-or-later
-*/
-
-#pragma once
-extern unsigned long GamePID;
